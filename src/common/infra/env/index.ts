1aa47e59433f97cbdb67bf06a233be942fef7e14
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-12-06 18:41:37 -0300

    setup for database

--- src/common/infra/env/index.ts
+++ src/common/infra/env/index.ts
@@ -2,17 +2,24 @@ import { AppError } from '@/common/domain/errors/app.error'
 import { z } from 'zod'
 
 const envSchema = z.object({
-	NODE_ENV: z
-		.enum(['development', 'test', 'production'])
-		.default('development'),
-	PORT: z.coerce.number().default(3000),
-	API_URL: z.string().default('http://localhost:3000'),
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().default(3000),
+  API_URL: z.string().default('http://localhost:3000'),
+  DB_TYPE: z.enum(['postgres', 'mysql', 'sqlite', 'mssql']).default('postgres'),
+  DB_USER: z.string().default('postgres'),
+  DB_PASS: z.string().default('docker'),
+  DB_HOST: z.string().default('localhost'),
+  DB_NAME: z.string().default('apivendas'),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_SCHEMA: z.string().default('public'),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-	throw new AppError('Invalid environment variables')
+  throw new AppError('Invalid environment variables')
 }
 
 export const env = _env.data
--- src/common/infra/http/server.ts
+++ src/common/infra/http/server.ts
@@ -2,5 +2,5 @@ import { env } from '../env'
 import { app } from './app'
 
 app.listen(env.PORT, () => {
-	console.log(`Server running on port ${env.PORT}`)
+  console.log(`Server running on port ${env.PORT}`)
 })
