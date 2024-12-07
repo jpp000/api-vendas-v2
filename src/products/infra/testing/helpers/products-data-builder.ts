71708f37b542c47ad98c25d2bf823dbc8e40cbcb
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-12-06 21:25:42 -0300

    product data builder

---
+++ src/products/infra/testing/helpers/products-data-builder.ts
@@ -0,0 +1,18 @@
+import { ProductModel } from '@/products/domain/models/product.model'
+import { faker } from '@faker-js/faker'
+import { randomUUID } from 'node:crypto'
+
+export function ProductsDataBuilder(
+  props: Partial<ProductModel>,
+): ProductModel {
+  return {
+    id: props.id ?? randomUUID(),
+    name: props.name ?? faker.commerce.productName(),
+    price:
+      props.price ??
+      Number(faker.commerce.price({ min: 100, max: 2000, dec: 2 })),
+    quantity: props.quantity ?? 10,
+    created_at: props.created_at ?? new Date(),
+    updated_at: props.updated_at ?? new Date(),
+  }
+}
