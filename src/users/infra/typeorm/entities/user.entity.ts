009b7f1ae8036621c7505e6faac1a7ca0dbb179c
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-12-09 14:58:30 -0300

    create users migration

---
+++ src/users/infra/typeorm/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import { UserModel } from '@/users/domain/models/user.model'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@Entity('users')
+export class User implements UserModel {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column('varchar')
+  name: string
+
+  @Column('varchar')
+  email: string
+
+  @Column('varchar')
+  password: string
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date
+}
